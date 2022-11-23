38c594342ffcd028484e5c94d224bf94dc343871
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-11-24 00:17:57 +0400

    refactoring

--- js/main.js
+++ js/main.js
@@ -11,17 +11,97 @@ const emptyList = `
 
 let tasks = [];
 
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.forEach(task => renderTask(task));
+  };
+
 checkIsEmpty();
 
 form.addEventListener('submit', addTask);
 taskList.addEventListener('click', removeTask);
+taskList.addEventListener('click', doneTask);
 
 function addTask(evt) {
   evt.preventDefault();
 
+  const taskText = taskInput.value;
+
+  let newTask = {
+    id: Date.now(),
+    text: taskText,
+    done: false,
+  };
+
+  if (!taskInput.value) return;
+  
+  renderTask(newTask);
+
+  tasks.push(newTask);
+        
+  taskInput.value = '';
+  taskInput.focus();
+
+  checkIsEmpty();
+  saveToStorage();
+};
+
+function removeTask(evt) {
+  if (evt.target.dataset.action !== "delete") return;
+
+  const parentNode = evt.target.closest('li');
+
+  let id = Number(parentNode.id);
+
+  // delete task from array
+  tasks = tasks.filter(function(task) {
+    if (task.id === id) {
+      return false;
+    } else {
+      return true;
+    };
+  });
+
+  parentNode.remove();
+
+  checkIsEmpty();
+  saveToStorage();
+};
+
+function doneTask(evt) {
+  if (evt.target.dataset.action !== 'done') return;
+  const parentItem = evt.target.closest('li');
+  const doneText = parentItem.querySelector('span');
+
+  let id = Number(parentItem.id);
+  const task = tasks.find((task) => task.id === id);
+  task.done === !task.done;
+
+  doneText.classList.toggle('class-done');
+
+  checkIsEmpty();
+  saveToStorage();
+};
+
+function checkIsEmpty() {
+  if (tasks.length === 0) {
+    taskList.insertAdjacentHTML('beforebegin', emptyList);
+  }
+
+  if (tasks.length > 0) {
+    const emptyItem = document.querySelector('#emptyList');
+    emptyItem ? emptyItem.remove() : null;
+  }
+
+  saveToStorage();
+};
+
+function renderTask(task) {
+  const doneClass = task.done ? 'task-done' : null;
+
   const newTask = `
-        <li class="bg-light rounded-5 d-flex p-3 mb-1"">
-          <span class="h4 m-0">${taskInput.value}</span>
+        <li class="bg-light rounded-5 d-flex p-3 mb-1" id="${task.id}">
+          <span class="${doneClass} h4 m-0">${task.text}</span>
           <div class="ms-auto ps-3 d-flex align-items-center gap-2 justify-content-center">
             <button class="btn btn-outline-success d-flex p-0" type="button" data-action="done">
               <ion-icon size="large" name="checkmark-done-outline"></ion-icon>
@@ -31,171 +111,10 @@ function addTask(evt) {
             </button>
           </div>  
         </li>`;
-
-  if (!taskInput.value) return;
-  else {
-    taskList.insertAdjacentHTML('beforeend', newTask);
-    tasks.push(newTask);
-    console.log(tasks);
-  }
-        
-  taskInput.value = '';
-  taskInput.focus();
-
-  checkIsEmpty();
+  
+  taskList.insertAdjacentHTML('beforeend', newTask);
 };
 
-function removeTask(evt) {
-  if (evt.target.dataset.action !== "delete") return;
-
-  const task = evt.target.closest('li');
-
-  task.remove();
-  tasks.pop(task);
-  console.log(tasks);
-  checkIsEmpty()
+function saveToStorage() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 };
-
-function checkIsEmpty() {
-  
-  if (tasks.length === 0) {
-    taskList.insertAdjacentHTML('beforebegin', emptyList);
-  }
-
-  if (tasks.length > 0) {
-    const emptyItem = document.querySelector('#emptyList');
-    emptyItem ? emptyItem.remove() : null;
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const form = document.querySelector('#form');
-// const taskInput = document.querySelector('#task-input');
-// const tasksList = document.querySelector('#tasksList');
-// const emptyListHTML = `
-//       <li class="list-group-item bg-light p-5 rounded-5 mb-1" id="emptyList">
-//       <div class="d-flex justify-content-center mb-5">
-//         <img width="100" src="./img/empty.svg" alt="Empty container icon.">
-//       </div>
-//       <p class="text-center fs-1 lh-1 m-0">Task list is empty</p>
-//       </li>`;
-
-// let tasks = [];
-
-// if (localStorage.getItem('tasks')) {
-//   tasks = JSON.parse(localStorage.getItem('tasks'));
-//   tasks.forEach(task => renderTask(task));
-// };
-
-// checkEmpty();
-
-// form.addEventListener('submit', addTask);
-// tasksList.addEventListener('click', deleteTask)
-// tasksList.addEventListener('click', doneTask)
-
-// function addTask(evt) {
-//   evt.preventDefault();
-
-//   const taskText = taskInput.value;
-//   const newTask = {
-//     id: Date.now(),
-//     text: taskText,
-//     done: false,
-//   }
-
-//   tasks.push(newTask);
-
-//   renderTask(newTask);
-
-//   taskInput.value = '';
-//   taskInput.focus();
-//   saveToLocalStorage();
-//   checkEmpty();
-// }
-
-// function deleteTask(evt) {
-//   if (evt.target.dataset.action !== 'delete') return;
-  
-//   const parentNode = evt.target.closest('li');
-//   const id = Number(parentNode.id);
-
-//   // delete task from array
-//   tasks = tasks.filter(function(task) {
-//     if (task.id === id) {
-//      return false;
-//     } else {
-//       return true;
-//     }
-//   });
-
-//   // delete task from layout
-//   parentNode.remove();
-//   checkEmpty();
-//   saveToLocalStorage();
-// }
-
-// function doneTask(evt) {
-//   if (evt.target.dataset.action !== 'done') return;
-
-//   const parentNode = evt.target.closest('li');
-//   const taskTitle = parentNode.querySelector('span');
-
-//   const id = Number(parentNode.id);
-//   const task = tasks.find((task) => task.id === id);
-
-//   task.done = !task.done;
-
-//   taskTitle.classList.toggle('task-done');
-//   saveToLocalStorage();
-//   checkEmpty();
-// }
-
-// function checkEmpty() {
-//   if (tasks.length === 0) {
-//     tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
-//   }
-
-//   if (tasks.length > 0) {
-//     const emptyList = document.querySelector('#emptyList');
-//     emptyList ? emptyList.remove() : null;
-//   }
-// }
-
-// function saveToLocalStorage() {
-//   localStorage.setItem('tasks', JSON.stringify(tasks))
-// }
-
-// function renderTask(task) {
-//   const cssClass = task.done ? 'task-done' : '';
-//   const taskHtml = `
-//         <li class="bg-light rounded-5 d-flex p-3 mb-1" id="${task.id}">
-//           <span class="${cssClass} h4 m-0">${task.text}</span>
-//           <div class="ms-auto ps-3 d-flex align-items-center gap-2">
-//             <button class="btn btn-outline-success" type="button" data-action="done">
-//               <img width="20" src="./img/checkmark.svg" alt="Check icon.">
-//             </button>
-//             <button class="btn btn-outline-danger" type="button" data-action="delete">
-//               <img width="20" src="./img/cross.svg" alt="Delete icon.">
-//             </button>
-//           </div>  
-//         </li>`;
-
-//   if (task.text) {
-//     tasksList.insertAdjacentHTML('beforeend', taskHtml);
-//   }
-// }
